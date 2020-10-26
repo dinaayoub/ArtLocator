77a2e44c7c26c18e8d791874ce89974c053fbd82
Dina Ayoub <dinaayoub@hotmail.com>
2020-10-26 13:23:13 -0700

    added the one table only schema if we need it

---
+++ data/oneTableOnlySchema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS artworks;
+
+CREATE TABLE artworks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT,
+    image VARCHAR(255),
+    artist VARCHAR(255).
+    museum VARCHAR(255),
+    city VARCHAR(255)
+);
+
+
+
